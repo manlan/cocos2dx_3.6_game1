193f8f3b6552e3dde7d1b7c537f6287a9a108ae9
manlan2010 <manlan2010@163.com>
2015-06-16 17:59:59 +0800

    2015年06月16日17:58:20         1 增加:主界面2

---
+++ Classes/CityScene.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mac on 15/6/16.
+//
+
+#include "CityScene.h"
---
+++ Classes/CityScene.h
@@ -0,0 +1,15 @@
+//
+// Created by Mac on 15/6/16.
+//
+
+
+#ifndef __CityScene_H_
+#define __CityScene_H_
+
+
+class CityScene {
+
+};
+
+
+#endif //__CityScene_H_
